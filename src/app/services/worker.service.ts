7eec45881d68ab4eecdba8aafa0d9224243c668b
prathmesh <prathmesh@.com>
2025-02-20 18:25:53 +0530

    push

---
+++ src/app/services/worker.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Worker } from '../models/worker';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorkerService {
+
+  private getUrl: string = "a64ee46a4ec6b47c085d286540d5afc0-347670287.us-east-1.elb.amazonaws.com:8080/api/v1/workers";
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getWorkers(): Observable<Worker[]> {
+    return this._httpClient.get<Worker[]>(this.getUrl).pipe(
+      map(response => response)
+    )
+  }
+
+  saveWorkers(worker: Worker): Observable<Worker> {
+    return this._httpClient.post<Worker>(this.getUrl, worker);
+  }
+
+  getWorker(id: Number): Observable<Worker> {
+    return this._httpClient.get<Worker>(`${this.getUrl}/${id}`).pipe(
+      map(response => response)
+    )
+  }
+
+  deleteWorker(id: Number): Observable<any> {
+    return this._httpClient.delete(`${this.getUrl}/${id}`, {responseType: 'text'});
+  }
+}
